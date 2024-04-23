fa54c783808d40d2aa78b7053bc6f19f882b4afe
mateidumitrescu <mateidum828@gmail.com>
2024-04-23 18:28:42 +0300

    first commit

---
+++ html/search/all_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['traversal_0',['Tree traversal',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['tree_1',['Tree',['../classtree_1_1_tree.html',1,'tree']]],
+  ['tree_20traversal_2',['Tree traversal',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['traversal_0',['Tree traversal',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['tree_20traversal_1',['Tree traversal',['../md__r_e_a_d_m_e.html',1,'']]]
+];
